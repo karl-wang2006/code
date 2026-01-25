179a072dfff07a696a2beaa4688dce29c832e95a
Xuanyu Wang <karlwang@sjtu.edu.cn>
2026-01-25 11:28:59 +0800

    lab

--- t1.cpp
+++ t1.cpp
@@ -1,23 +1,19 @@
-//#include<iostream>
-//#include<vector>
-//#include <string>   
-//#include <sstream> 
-//#include <fstream>
-//#include <algorithm>
-//using namespace std;
-//
-//int main()
-//{
-//	int N,ret=0;
-//	cout << "Enter N: ";
-//	cin >> N;
-//	for (int i = 1; i < N / 2 + 1; i++)
-//	{
-//		if (N % i == 0)
-//		{
-//			ret += i;
-//		}
-//	}
-//	cout << "Sum: " << ret << endl;
-//	return 0;
-//}
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int N, ret = 0;
+    cout << "Enter N: ";
+    cin >> N;
+    for (int i = 1; i < N / 2 + 1; i++)
+    {
+        if (N % i == 0)
+        {
+            ret += i;
+        }
+    }
+    cout << "Sum: " << ret << endl;
+    return 0;
+}	
+
